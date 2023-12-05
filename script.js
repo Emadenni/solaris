cbd984ae3bf13a3d2062ca3fe8d2546b2fee15f6
Emadenni <emanyele.dng86@gmail.com>
2023-12-05 22:26:42 +0100

    added intro, fixed layout issues, added comment

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
+/* --------------------- global variables------------------- */
 let planetData = [];
 let selectedPlanet;
+const startBtn = document.querySelector("#startButton");
+const intro = document.querySelector("#introContainer");
+const mainPage = document.querySelector(".mainContainer");
 
-/*------------------------- FUNCTION : getPlanetInfo ------------------------*/
-document.addEventListener('DOMContentLoaded', () => {
-  getClickableList();  
+/* --------------------- main event/page load-------------------- */
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* --------------------- EVENT LISTENER: intro/start button ------------ */
+
+  startBtn.addEventListener("click", () => {
+    intro.style.display = "none";
+    mainPage.style.display = "block";
+  });
+  getClickableList();
   getPlanetInfo();
-  generateStarField(); // Chiamiamo la funzione per generare le stelle iniziali
+  generateStarField();
 
-  function getApiKey() {
-    return fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
-      method: "POST",
-      headers: { "x-zocom": "" },
-    })
-      .then(response => {
+  const spaceSound = document.getElementById("spaceSound");
+  spaceSound.addEventListener("canplaythrough", () => {
+    console.log("Audio ready");
+  });
+
+  /* --------------------- ASYNC FUNCTION : getApyKey()------------ */
+  //obtains and returns the keys to the API
+  async function getApiKey() {
+    return fetch(
+      "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys",
+      {
+        method: "POST",
+        headers: { "x-zocom": "" },
+      }
+    )
+      .then((response) => {
         if (!response.ok) {
-          throw new Error(`Error during Api request. Status: ${response.status}`);
+          throw new Error(
+            `Error during Api request. Status: ${response.status}`
+          );
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         console.log("Key gotten:", data.key);
         return data.key;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error during Api request:", error);
         throw error;
       });
   }
 
+  /* --------------------- ASYNC FUNCTION : getPlanetInfo()------------ */
+  //uses the API keys to fetch informations about planets
+
   async function getPlanetInfo() {
     try {
       const apiKey = await getApiKey();
       console.log("Api-key used:", apiKey);
-      const apiUrl = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies";
+      const apiUrl =
+        "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies";
       const response = await fetch(apiUrl, {
         method: "GET",
         headers: { "x-zocom": apiKey },
@@ -57,18 +84,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  /* ---------------------FUNCTION : getClickableList()------------ */
+  //encloses the elements with class "toClick" in an array
+
   function getClickableList() {
-    const clickableElements = document.querySelectorAll('.toClick');
+    const clickableElements = document.querySelectorAll(".toClick");
     return Array.from(clickableElements);
   }
+
+  /* ---------------------FUNCTION : attachClickEvents()------------ */
+  // creates a loop through clickable elements
+  // assign a click EVENT LISTENER to each clickable element
+  // compares and matches the index of the array of clickable elements with the index of the array returned by the API
+  // encloses the HTML content of the overlay in a variable with the dynamic parts entrusted to template literals
+  // encloses the HTML content of the overlay in a variable
 
   function attachClickEvents() {
     const clickableList = getClickableList();
     clickableList.forEach((element, index) => {
-      element.addEventListener('click', () => {
-        console.log("Index:", index);
-        console.log("Planet Data Length:", planetData.length);
-
+      element.addEventListener("click", () => {
         if (planetData && planetData.length > index) {
           selectedPlanet = planetData[index];
           console.log("Selected Planet:", selectedPlanet);
@@ -76,8 +110,13 @@ document.addEventListener('DOMContentLoaded', () => {
           const overlayContent = `  <div class="overlay" id="pageOverlay">
             <button id="goBackBtn">&leftarrow;</button>
             <div class=" planetsImg  ${selectedPlanet.name.toLowerCase()}"></div>
-            ${selectedPlanet.name.toLowerCase() === 'saturnus' ? '<div class="ring2"></div>' : ''}
+            ${
+              selectedPlanet.name.toLowerCase() === "saturnus"
+                ? '<div class="ring2"></div>'
+                : ""
+            }
             <div class="planetInfo">
+            
               <div class="titlesBoxOverlay">
                 <h1>${selectedPlanet.name}</h1>
                 <h2>${selectedPlanet.latinName}</h2>
@@ -109,70 +148,93 @@ document.addEventListener('DOMContentLoaded', () => {
               <hr>
               <div class="moons details">
                 <h3>MÅNAR</h2>
-                <P>${selectedPlanet.moons.length > 0 ? selectedPlanet.moons.join(', ') : 'Noll'}</P>
+                <P>${
+                  selectedPlanet.moons.length > 0
+                    ? selectedPlanet.moons.join(", ")
+                    : "Noll"
+                }</P>
               </div>
             </div>
           </div>`;
 
-          document.body.innerHTML = '';
+          //makes the body empty and fills it with the generated overlay
+
+          document.body.innerHTML = "";
           document.body.innerHTML = overlayContent;
 
-          const goBackBtn = document.getElementById("goBackBtn");
+          /* ---------------------EVENT LISTENER : goBack button------------ */
+          // remove the overlay
+          // makes the body empty again
+          // show the mainPageùs content
 
+          const goBackBtn = document.getElementById("goBackBtn");
           if (goBackBtn) {
-            goBackBtn.addEventListener('click', () => {
-              location.reload();
+            goBackBtn.addEventListener("click", () => {
+              document.body.removeChild(pageOverlay);
+
+              if (mainPage) {
+                document.body.innerHTML = "";
+                document.body.appendChild(mainPage);
+                generateStarField(); //re-creates stars bkg for the mainPage
+              }
             });
           }
-
-          generateStarField(); // Chiamiamo la funzione per generare le stelle dopo l'overlay
+          generateStarField(); //create stars bkg for the overlay
         } else {
           console.error("Planet data undefined or index out of bounds");
+        }
+
+        if (spaceSound) {
+          spaceSound.play(); //play a sound when a planet infos are showed
         }
       });
     });
   }
+
+  /* ---------------------FUNCTION : attachHoverEvents()------------ */
+  // creates 2 EVENT LISTENER (mouseover,mouseon) to handle the planets name preview
+  // creates a container where to place the preview
+  // uses a similar method used in click event listener
 
   function attachHoverEvents() {
     const clickableList = getClickableList();
     let previewCreated = false;
 
     clickableList.forEach((element, index) => {
-      element.addEventListener('mouseover', () => {
-        console.log('Mouse over planet');
+      element.addEventListener("mouseover", () => {
+        console.log("Mouse over planet");
         if (planetData && planetData.length > index && !previewCreated) {
           selectedPlanet = planetData[index];
+          const previewContainer = document.createElement("div");
+          previewContainer.classList.add("preview-container");
           const planetName = document.createElement("h3");
           planetName.innerText = selectedPlanet.name;
           planetName.classList.add("preview");
-
-          const previewContainer = document.createElement("div");
-          previewContainer.classList.add("preview-container");
           previewContainer.appendChild(planetName);
-
-          // Inserisci l'anteprima subito prima dell'elemento di riferimento (il pianeta)
-          element.insertAdjacentElement('beforebegin', previewContainer);
-
-          previewCreated = true; // Imposta il flag per indicare che l'anteprima è stata creata
+          document.body.appendChild(previewContainer);
+          previewCreated = true; //flag that shows that the preview is created
         }
       });
 
-      element.addEventListener('mouseout', () => {
-        // Rimuovi l'anteprima solo se è stata creata
+      element.addEventListener("mouseout", () => {
+        // remove preview only if it has been created (that's why the flag)
         if (previewCreated) {
-          const previewContainer = element.previousSibling;
+          const previewContainer = document.querySelector(".preview-container");
           if (previewContainer) {
             previewContainer.remove();
           }
-          previewCreated = false; // Resetta il flag quando l'anteprima viene rimossa
+          previewCreated = false; // reset the plag on "removed"
         }
       });
     });
   }
 
+  /* ---------------------FUNCTION : generateStarField(------------ */
+  // creats 100 stars and distributes them in the created container in random position and size (getRandomNumber())
+
   function generateStarField() {
-    const starContainer = document.createElement('div');
-    starContainer.id = 'star-container';
+    const starContainer = document.createElement("div");
+    starContainer.id = "star-container";
 
     const numberOfStars = 100;
 
